3432441d49194334b58735ad88d328c0d762af21
smalykhin <v-smalykhin@expediagroup.com>
2020-02-04 15:46:03 +0200

    added spring-cloud-starter-sleuth to trace logs via multiple services by it's id sample: [<service-name>,<id comes from>,<id changed to, in the current service>,true]

--- currency-convert-service/src/main/java/com/currency/currencyconvertservice/CurrencyConvertServiceApplication.java
+++ currency-convert-service/src/main/java/com/currency/currencyconvertservice/CurrencyConvertServiceApplication.java
@@ -1,9 +1,11 @@
 package com.currency.currencyconvertservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients("com.currency.currencyconvertservice")
@@ -12,6 +14,11 @@ public class CurrencyConvertServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConvertServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
